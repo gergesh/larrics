46c567946556c8c54e14e30df563e9ddaea7b213
Yoav Shai <gergesh@gmail.com>
2020-06-20 22:11:30 +0300

    Update quicklyric provider
    The previous one is no longer used by the app and is missing lyrics for
    some songs.

--- larrics/providers/quicklyric.py
+++ larrics/providers/quicklyric.py
@@ -1,15 +1,12 @@
-import json
-import re
-import time
+import datetime as dt
+import hmac
+from base64 import b64encode
+from hashlib import sha1
 
 import requests
-import urllib3
 
-from ..lyrics import Lyrics, strip_timestamps
+from ..lyrics import Lyrics
 from .provider import Provider
-
-# QuickLyric's certificate is invalid and requests keeps complaining about it
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class QuickLyric(Provider):
@@ -17,48 +14,45 @@ class QuickLyric(Provider):
     synchronized = True
 
     def __init__(self, config_entry):
-        self.token = config_entry['token']
+        self.s = requests.Session()
+        self.s.headers.update(
+            {'user-agent': 'com.geecko.QuickLyric', 'accept-encoding': 'gzip'}
+        )
+        self.location = (
+            config_entry.get('location')
+            or self.s.head('https://api.quicklyric.com').headers['location']
+        )
+        self.zero_duration = config_entry.getboolean('zero_duration', False)
+        self.player = config_entry.get('player', 'com.poupa.vinylmusicplayer')
 
     def get_lyrics(self, artist: str, title: str, duration: int) -> Lyrics:
-        url = 'https://api.quicklyric.be/get?lrc=true'
-        headers = {
-            'User-Agent': 'com.geecko.QuickLyric',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-        payload = {
-            'q': '\n'.join(
-                [artist, title, str(int(time.time() * 1000)), '4000260', self.token,]
-            ),
-            'v': '4000260',
-            'p': 'com.poupa.vinylmusicplayer',
-            'au': 'false',
-            'sag': 'true',
-        }
+        params = [
+            ('q_track', title),
+            ('q_artist', artist),
+            ('q_fingerprint', ''),
+            ('q_duration', '0' if self.zero_duration else str(duration)),
+            ('q_sag', 'false'),
+            ('player', self.player),
+            ('v', '4000310'),
+        ]
+        signature = self._get_signature(
+            f'/?{"&".join(f"{k}={v}" for k, v in params)}'.replace(' ', '+'),
+            b'a22ddff2b3d447e25705ac779abfcee98e5b8756',
+        )
+        params.append(('signature', signature))
+        params.append(('signature_protocol', 'sha1'))
+        j = self.s.get(f'{self.location}matchLyrics', params=params).json()
+        return Lyrics(j.get('text'), self._lrc_from_lines(j.get('lines')))
 
-        if duration:
-            payload['d'] = str(duration)
-        r = requests.post(url, headers=headers, data=payload, verify=False)
-        if r.status_code != 200:
-            raise Exception('QuickLyric: Got a non-200 status code')
+    @staticmethod
+    def _get_signature(params: str, hmac_init: bytes) -> bytes:
+        now = dt.datetime.now(dt.timezone.utc)
+        dat = f'{params}{now.year}{now.month:02}{now.day:02}'
+        mac = hmac.new(hmac_init, dat.encode('utf-8'), sha1)
+        return b64encode(mac.digest()).rstrip(b'=')
 
-        js = json.loads(r.text)
-        lyrics = Lyrics()
-        if js['code'] != 200:
-            return lyrics
-        if js['LRC']:  # Synchronized lyrics
-            return Lyrics(strip_timestamps(js['text']), js['text'])
-        else:
-            return Lyrics(self.txt_from_html(js['text']), None)
-
-    _re_multiple_newlines = re.compile(r'\n{2,}')
-    _re_trailing_spaces = re.compile(r'\s+\n')
-    _re_html_newline = re.compile(r'<(br|p)>')
-    _re_html_tag = re.compile(r'</?\w+>')
-
-    @classmethod
-    def txt_from_html(cls, x):
-        x = cls._re_html_newline.sub('\n', x)
-        x = cls._re_html_tag.sub('', x)
-        x = cls._re_multiple_newlines.sub('\n', x)
-        x = cls._re_trailing_spaces.sub('\n', x)
-        return x.strip()
+    @staticmethod
+    def _lrc_from_lines(lines: []) -> str:
+        if not lines:
+            return None
+        return '\n'.join(f'[{l["timing"]}]{l["text"]}' for l in lines)
